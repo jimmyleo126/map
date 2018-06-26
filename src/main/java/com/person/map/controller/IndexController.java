3eb19264a483b770af553e473568a9c49c7676a8
jimmyleo <jimmyleo126@163.com>
2018-06-26 18:05:56 +0800

    前端页面搭建，就等后端代码填充

---
+++ src/main/java/com/person/map/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.person.map.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String getindex() {
+		return "blog/page/index";
+	}
+
+}
