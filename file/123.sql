09541002ab0e3b247ae045196220ad3e315eff0f
jimmyleo <jimmyleo126@163.com>
2018-06-27 18:18:15 +0800

    文件

---
+++ file/123.sql
@@ -0,0 +1,71 @@
+/*
+SQLyog Ultimate v12.08 (64 bit)
+MySQL - 5.7.22 : Database - lj
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`lj` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `lj`;
+
+/*Table structure for table `t_contents` */
+
+DROP TABLE IF EXISTS `t_contents`;
+
+CREATE TABLE `t_contents` (
+  `cid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) DEFAULT NULL,
+  `titlePic` varchar(55) DEFAULT NULL,
+  `slug` varchar(200) DEFAULT NULL,
+  `created` int(10) unsigned DEFAULT '0',
+  `modified` int(10) unsigned DEFAULT '0',
+  `content` text COMMENT '内容文字',
+  `authorId` int(10) unsigned DEFAULT '0',
+  `type` varchar(16) DEFAULT 'post',
+  `status` varchar(16) DEFAULT 'publish',
+  `tags` varchar(200) DEFAULT NULL,
+  `categories` varchar(200) DEFAULT NULL,
+  `hits` int(10) unsigned DEFAULT '0',
+  `commentsNum` int(10) unsigned DEFAULT '0',
+  `allowComment` tinyint(1) DEFAULT '1',
+  `allowPing` tinyint(1) DEFAULT '1',
+  `allowFeed` tinyint(1) DEFAULT '1',
+  PRIMARY KEY (`cid`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `created` (`created`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `t_contents` */
+
+/*Table structure for table `t_users` */
+
+DROP TABLE IF EXISTS `t_users`;
+
+CREATE TABLE `t_users` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '用户自增编号',
+  `username` varchar(32) NOT NULL COMMENT '用户姓名',
+  `password` varchar(64) NOT NULL COMMENT '用户密码',
+  `email` varchar(200) NOT NULL COMMENT '用户邮箱',
+  `homeurl` varchar(200) DEFAULT NULL COMMENT '用户主页链接',
+  `nickname` varchar(60) DEFAULT NULL COMMENT '用户昵称',
+  `created` int(10) unsigned NOT NULL DEFAULT '0',
+  `activated` int(10) unsigned NOT NULL DEFAULT '0',
+  `logged` int(10) unsigned NOT NULL DEFAULT '0',
+  `groupname` varchar(16) NOT NULL DEFAULT 'visitor' COMMENT '用户分组名',
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `t_users` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
